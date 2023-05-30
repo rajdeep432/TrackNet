71d1d962725d4de54f63005267f43f29d8a1cf53
Rajdeep Das <120500013+rajdeep432@users.noreply.github.com>
2023-05-31 00:21:31 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,34 +2,31 @@ from argparse import ArgumentParser
 from TrackNet import TrackNet
 import torchvision
 import torch
-import cv2 as cv
-
+import cv2
 
 def get_ball_position(img, opt, original_img_=None):
-    ret, thresh = cv.threshold(img, opt.brightness_thresh, 1, 0)
-    thresh = cv.convertScaleAbs(thresh)
+    ret, thresh = cv2.threshold(img, opt.brightness_thresh, 1, 0)
+    thresh = cv2.convertScaleAbs(thresh)
 
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     if len(contours) != 0:
-
-        #find the biggest area of the contour
-        c = max(contours, key = cv.contourArea)
+        # Find the biggest area of the contour
+        c = max(contours, key=cv2.contourArea)
 
         if original_img_ is not None:
-            # the contours are drawn here
-            cv.drawContours(original_img_, [c], -1, 255, 3)
+            # Draw the contours
+            cv2.drawContours(original_img_, [c], -1, 255, 3)
 
-        x,y,w,h = cv.boundingRect(c)
+        x, y, w, h = cv2.boundingRect(c)
         return x, y, w, h
-
 
 def parse_opt():
     parser = ArgumentParser()
     parser.add_argument('video', type=str, default='video.mp4', help='Path to video.')
     parser.add_argument('--save_path', type=str, default='predicted.mp4', help='Path to result video.')
     parser.add_argument('--weights', type=str, default='weights', help='Path to trained model weights.')
-    parser.add_argument('--sequence_length', type=int, default=3, help='Length of the images sequence used as X.')
+    parser.add_argument('--sequence_length', type=int, default=3, help='Length of the image sequence used as X.')
     parser.add_argument('--image_size', type=int, nargs=2, default=[512, 1024], help='Size of the images used for training (y, x).')
     parser.add_argument('--device', type=str, default='cpu', help='Device to use (cpu, cuda, mps).')
     parser.add_argument('--one_output_frame', action='store_true', help='Demand only one output frame instead of three.')
@@ -45,18 +42,18 @@ if __name__ == '__main__':
     opt.dropout = 0
     device = torch.device(opt.device)
     model = TrackNet(opt).to(device)
-    model.load(opt.weights, device = opt.device)
+    model.load_state_dict(torch.load(opt.weights, map_location=device)['model_state_dict'])
     model.eval()
 
-    cap = cv.VideoCapture(opt.video)
+    cap = cv2.VideoCapture(opt.video)
     videoEndReached = False
 
     while cap.isOpened():
-        #TODO: add one_output_frame support
+        # TODO: add one_output_frame support
 
         frames = []
         for _ in range(opt.sequence_length):
-            ret,frame = cap.read()
+            ret, frame = cap.read()
             if not ret:
                 videoEndReached = True
                 break
@@ -74,19 +71,18 @@ if __name__ == '__main__':
         frames_torch = torch.cat(frames_torch, dim=0).unsqueeze(0)
 
         pred = model(frames_torch)
-        pred = pred[0,:,:,:].detach().cpu().numpy()
+        pred = pred[0, :, :, :].detach().cpu().numpy()
 
         for i in range(opt.sequence_length):
-            pred_frame = pred[i,:,:]
-            pred_frame = cv.resize(pred_frame, (frames[i].shape[1], frames[i].shape[0]), interpolation = cv.INTER_AREA)
+            pred_frame = pred[i, :, :]
+            pred_frame = cv2.resize(pred_frame, (frames[i].shape[1], frames[i].shape[0]), interpolation=cv2.INTER_AREA)
             get_ball_position(pred_frame, opt, original_img_=frames[i])
-            cv.imshow('prediction', pred_frame)
-            cv.imshow('original', frames[i])
-            cv.waitKey(opt.waitBetweenFrames)
+            cv2.imshow('prediction', pred_frame)
+            cv2.imshow('original', frames[i])
+            cv2.waitKey(opt.waitBetweenFrames)
 
-        if cv.waitKey(10) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-
     cap.release()
-    cv.destroyAllWindows() # destroy all opened windows
+    cv2.destroyAllWindows()  # destroy all opened windows
